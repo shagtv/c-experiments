6b77f7ef57af3008a49ff3df542b039ca9fb3968
Timur Shagiakhmetov <timur.shagiakhmetov@corp.badoo.com>
2017-11-14 18:24:16 +0000

    added hello world program

---
+++ hello_world/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	std::cout << "Arguments:" << std::endl;
+	
+	for (int i = 0; i < argc; i++) {
+		std::cout << argv[i] << std::endl;
+	}
+	
+	return 0;
+}
+
